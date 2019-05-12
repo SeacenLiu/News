2f55253631802c2e7a23ede0f7daf7c84937d559
SeacenLiu <1016667972@qq.com>
2019-05-12 20:08:01 +0800

    WebView添加进度条——ProgressWebView

---
+++ app/src/main/java/com/example/seacen/news/News/ProgressWebView.java
@@ -0,0 +1,67 @@
+package com.example.seacen.news.News;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ClipDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
+
+public class ProgressWebView  extends WebView  {
+    private ProgressBar progressbar;
+
+    public ProgressWebView(Context context) {
+        super(context);
+    }
+
+    public ProgressWebView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public ProgressWebView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initProgressBar(context);
+        setWebChromeClient(new WebChromeClient());
+    }
+
+    private void initProgressBar(Context context) {
+        progressbar = new ProgressBar(context, null, android.R.attr.progressBarStyleHorizontal);
+        progressbar.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, dp2px(context, 3), 0, 0));
+        //改变progressbar默认进度条的颜色（深红色）为Color.GREEN
+        progressbar.setProgressDrawable(new ClipDrawable(new ColorDrawable(Color.GREEN), Gravity.LEFT, ClipDrawable.HORIZONTAL));
+        addView(progressbar);
+    }
+
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        super.onScrollChanged(l, t, oldl, oldt);
+    }
+
+    /**
+     * 方法描述：根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public int dp2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 类描述：显示WebView加载的进度情况
+     */
+    public class WebChromeClient extends android.webkit.WebChromeClient {
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            if (newProgress == 100) {
+                progressbar.setVisibility(GONE);
+            } else {
+                if (progressbar.getVisibility() == GONE)
+                    progressbar.setVisibility(VISIBLE);
+
+                progressbar.setProgress(newProgress);
+            }
+            super.onProgressChanged(view, newProgress);
+        }
+    }
+}
